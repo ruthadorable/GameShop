de3a622eff0741e4649283083bb92ef1831ba2d6
ruthadorable <ruthadorable@live.com>
2020-05-03 07:36:25 +0200

    Create wii.php

---
+++ PHP_Files/wii.php
@@ -0,0 +1,92 @@
+<h1>Consoles PS4</h1>
+
+<div class="row">
+    <div class="col-sm-4">
+        <div class="card">
+            <div class="card-header"><img src="images/wiipackjeuxconsole.jpg" width='425px' height='250px'class="img-fluid"></div>
+            <div class="card-body">
+                <h2>Console Wii + 2 jeux</h2>
+                <p>Prix: 164.99 euros </p>
+                
+            </div>
+            <div class="card-footer">
+
+
+                <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo">Plus d'infos</button>
+                <div id="demo" class="collapse">
+                    <br><br>
+                    <h4>Caractéristiques techniques</h4><br>
+                        
+
+
+                        <p>Plateforme:</p>
+                        Nintendo Wii<br>
+                        <p>Ecran Vidéo:</p>
+                        TV via peritel<br>
+                        <p>Connexion internet:</p>
+                        Oui<br>
+                        <p>Type de connexion:</p>
+                        Haut débit (LAN)<br>
+                        <p>Communication sans fil:</p>
+                        Wi-Fi 802.11 b/g<br>
+
+                        <p>Nombre de ports pour périphériques:</p>
+                        4<br>
+                        <p>Mode multi joueurs:</p>
+                        Quatre ports pour manettes Nintendo GameCuble<br>
+                        <p>Standard:</p>
+                        PAL<br>
+                        <p>Caractéristiques complémentaires:</p>
+                        Lecteur de de disques optiques de 12 cm à simple ou double couche et 8 cm (pour jeux GameCube), service WiiConnect24 de connexion Internet lorsque la console est éteinte, possibilité de communication sans fil avec la Nintendo DS
+
+
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="card">
+            <div class="card-header"><img src="images/wiiplatformred.jpg"class="img-fluid" ></div>
+                <div class="card-body">
+                    
+                    <h3>Console Wii rouge Nintendo Pack Anniversaire 25 ans + New Super Mario Bros</h3>
+                    Prix: 165.80 euros
+                    
+
+
+                </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo">Plus d'infos</button>
+                <div id="demo" class="collapse">
+                    <br><br>
+                    <h4>Caractéristiques techniques</h4><br>
+                       
+                        <p>Plateforme:</p>
+                        Nintendo Wii<br>
+                        <p>Ecran Vidéo:</p>
+                        TV via peritel<br>
+                        <p>Connexion internet:</p>
+                        Oui<br>
+                        <p>Type de connexion:</p>
+                        Haut débit (LAN)<br>
+                        <p>Communication sans fil:</p>
+                        Wi-Fi 802.11 b/g<br>
+
+                        <p>Nombre de ports pour périphériques:</p>
+                        4<br>
+                        <p>Mode multi joueurs:</p>
+                        Quatre ports pour manettes Nintendo GameCuble<br>
+                        <p>Standard:</p>
+                        PAL<br>
+                        <p>Caractéristiques complémentaires:</p>
+                        Lecteur de de disques optiques de 12 cm à simple ou double couche et 8 cm (pour jeux GameCube), service WiiConnect24 de connexion Internet lorsque la console est éteinte, possibilité de communication sans fil avec la Nintendo DS
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
