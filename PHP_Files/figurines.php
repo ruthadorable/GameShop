136e4dce3bd07d7ea16e606f0e965b7462b26c1d
ruthadorable <ruthadorable@live.com>
2020-05-03 07:10:23 +0200

    Create figurines.php

---
+++ PHP_Files/figurines.php
@@ -0,0 +1,25 @@
+<style>
+    .row img{
+        width:300px;
+        height: 250px;
+    }
+    .container-fluid{
+        float: centered;
+    }
+
+</style>
+
+
+
+<div class="container-fluid">
+    <h1>Figurines</h1>
+    <p>Les images ici sont adaptable à l'écran.</p>
+
+    <div class="row">
+        <div class="col-sm4" ><img src="images/league-of-legends-figurine-pop-ashe.jpg"alt="figurine ashe prix:11euros " class='img-fluid'></div>
+        <div class="col-sm4" ><img src="images/mini-figurine-ashe.jpg" alt="figurine ashe prix:10euros" class='img-fluid'></div>
+        <div class="col-sm4" ><img src="images/one-punch-man-figurine-pop-genos.jpg" alt="figurine punch man prix: 11euros" class='img-fluid'></div>
+        <div class="col-sm4" ><img src="images/stranger-things-figurine-pop-brenner.jpg"alt="figurine brenner prix: 11 euros" class='img-fluid'></div>
+    </div>
+</div>
+
