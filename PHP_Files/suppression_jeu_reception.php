40f6268b127775b68a95a14ce0935589b97e0dcf
ruthadorable <ruthadorable@live.com>
2020-05-03 07:26:52 +0200

    Create suppression_jeu_reception.php

---
+++ PHP_Files/suppression_jeu_reception.php
@@ -0,0 +1,15 @@
+<?php
+//COMPLETER
+?>
+
+<h3>Réception des données GET (id) : var_dump puis enregistrement de l'id dans une variable</h3>
+<?php
+//COMPLETER
+?>
+
+<h3>DELETE en DB du spectacle n°<?php echo($id); ?></h3>
+
+<?php
+//COMPLETER
+?>
+
